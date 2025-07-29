11796df19f5125bec22b556a49a78b8f5579df2d
Ananda Nur Hisanah <nurhisanah@gmail.com>
2025-07-29 19:27:02 +0800

    build: traits uuid

---
+++ app/Traits/HasUuid.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait HasUuid
+{
+    /**
+     * Boot the trait and attach creating event listener.
+     */
+    protected static function bootHasUuid()
+    {
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                // UUID v7
+                $model->uuid = Uuid::uuid7()->toString();
+            }
+        });
+    }
+}
