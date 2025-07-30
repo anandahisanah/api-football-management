2d9883d95e89de9d89c8e91928a71a86c02181cc
Ananda Nur Hisanah <nurhisanah@gmail.com>
2025-07-30 22:44:00 +0800

    build: auth

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use App\Models\User;
+use DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+     /**
+     * @unauthenticated
+     *
+     * Login.
+     *
+     * Login.
+     *
+     */
+    public function login(LoginRequest $request)
+    {
+        try {
+            $request->validated();
+
+            DB::beginTransaction();
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthorized'
+                ], 401);
+            }
+
+            // find user
+            $user = User::where('email', $request->email)->firstOrFail();
+
+            // create token
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Success',
+                'data' => [
+                    'token_type' => 'Bearer',
+                    'access_token' => $token,
+                    'user' => $user,
+                ],
+            ], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => 'error',
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     *
+     * Logout.
+     *
+     * Logout the authenticated user.
+     *
+     */
+    public function logout(Request $request)
+    {
+        try {
+            if (!$request->user()) {
+                return response()->json([
+                    'status'  => 'error',
+                    'message' => 'Unauthenticated.',
+                ]);
+            }
+
+            // delete token
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->json([
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Logged out successfully'
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => $th->getCode(),
+                'status' => 'error',
+                'message' => $th->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     *
+     * Update Password.
+     *
+     * Update Password the authenticated user.
+     *
+     */
+    public function updatePassword(Request $request)
+    {
+        try {
+            $request->validate([
+                /**
+                 * Current password
+                 *
+                 * @example string
+                 */
+                'current_password' => 'required|string',
+                /**
+                 * New password
+                 *
+                 * @example string
+                 */
+                'new_password' => 'required|string|confirmed',
+            ]);
+
+            $user = Auth::user();
+
+            // verified the new password
+            if (!Hash::check($request->input('current_password'), $user->password)) {
+                return response()->json([
+                    'code' => 400,
+                    'status' => 'error',
+                    'message' => 'Current password is incorrect',
+                ], 400);
+            }
+
+            // update
+            $user->password = Hash::make($request->input('new_password'));
+            $user->save();
+
+            return response()->json([
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Password updated successfully',
+            ]);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => $th->getCode() ?: 500,
+                'status' => 'error',
+                'message' => $th->getMessage(),
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,46 +1,52 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\PlayerController;
 use App\Http\Controllers\Api\ReportController;
 use App\Http\Controllers\Api\ScoreController;
 use App\Http\Controllers\Api\TeamController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// auth
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::prefix('team')->name('team')->group(function () {
-    Route::get('/', [TeamController::class, 'index']);
-    Route::get('/show', [TeamController::class, 'show'])->name('.show');
-    Route::post('/store', [TeamController::class, 'store'])->name('.store');
-    Route::post('/update', [TeamController::class, 'update'])->name('.update');
-    Route::delete('/delete/{id}', [TeamController::class, 'delete'])->name('.delete');
-});
+Route::middleware('auth:sanctum')->group(function () {
+    // logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // update password
+    Route::post('/update-password', [AuthController::class, 'updatePassword']);
+    
+    Route::prefix('team')->name('team')->group(function () {
+        Route::get('/', [TeamController::class, 'index']);
+        Route::get('/show', [TeamController::class, 'show'])->name('.show');
+        Route::post('/store', [TeamController::class, 'store'])->name('.store');
+        Route::post('/update', [TeamController::class, 'update'])->name('.update');
+        Route::delete('/delete/{id}', [TeamController::class, 'delete'])->name('.delete');
+    });
 
-Route::prefix('player')->name('player')->group(function () {
-    Route::get('/', [PlayerController::class, 'index']);
-    Route::get('/show', [PlayerController::class, 'show'])->name('.show');
-    Route::post('/store', [PlayerController::class, 'store'])->name('.store');
-    Route::post('/update', [PlayerController::class, 'update'])->name('.update');
-    Route::delete('/delete/{id}', [PlayerController::class, 'delete'])->name('.delete');
-});
+    Route::prefix('player')->name('player')->group(function () {
+        Route::get('/', [PlayerController::class, 'index']);
+        Route::get('/show', [PlayerController::class, 'show'])->name('.show');
+        Route::post('/store', [PlayerController::class, 'store'])->name('.store');
+        Route::post('/update', [PlayerController::class, 'update'])->name('.update');
+        Route::delete('/delete/{id}', [PlayerController::class, 'delete'])->name('.delete');
+    });
 
-Route::prefix('game')->name('game')->group(function () {
-    Route::get('/', [GameController::class, 'index']);
-    Route::get('/show', [GameController::class, 'show'])->name('.show');
-    Route::post('/store', [GameController::class, 'store'])->name('.store');
-    Route::post('/update', [GameController::class, 'update'])->name('.update');
-    Route::delete('/delete/{id}', [GameController::class, 'delete'])->name('.delete');
-});
+    Route::prefix('game')->name('game')->group(function () {
+        Route::get('/', [GameController::class, 'index']);
+        Route::get('/show', [GameController::class, 'show'])->name('.show');
+        Route::post('/store', [GameController::class, 'store'])->name('.store');
+        Route::post('/update', [GameController::class, 'update'])->name('.update');
+        Route::delete('/delete/{id}', [GameController::class, 'delete'])->name('.delete');
+    });
 
-Route::prefix('score')->name('score')->group(function () {
-    Route::post('/store', [ScoreController::class, 'store'])->name('.store');
-    Route::delete('/delete/{id}', [ScoreController::class, 'delete'])->name('.delete');
-});
+    Route::prefix('score')->name('score')->group(function () {
+        Route::post('/store', [ScoreController::class, 'store'])->name('.store');
+        Route::delete('/delete/{id}', [ScoreController::class, 'delete'])->name('.delete');
+    });
 
-Route::prefix('report')->name('report')->group(function () {
-    Route::get('/', [ReportController::class, 'index']);
+    Route::prefix('report')->name('report')->group(function () {
+        Route::get('/', [ReportController::class, 'index']);
+    });
 });
